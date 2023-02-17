a2705400f665665f9bfd236c9c16dc8c741d9740
Rohan Kewalramani <rkewalramani4@gmail.com>
2023-02-17 16:49:50 -0500

    initial push for project

---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="text-center p-5">
+      <h1 className="text-2xl font-bold text-white">Recession Count Down</h1>
+    </div>
+  );
+};
+
+export default Header;
