c417772ea6ebadde9aa6491ea8d22ddefa791a23
Rohan Kewalramani <rkewalramani4@gmail.com>
2023-02-17 21:11:17 -0500

    feat: created Box component for stocks

---
+++ src/Box.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Box = () => {
+  return (
+    <div className="box-content h-32 w-64 p-4 border-2 rounded-lg border-black">
+      <div className="text-center font-bold text-2xl">TSLA</div>
+    </div>
+  );
+};
+
+export default Box;
