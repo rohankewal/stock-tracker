45c9168f84fa150d8614e45fdaeccd8e9d57e7a5
Rohan Kewalramani <rkewalramani4@gmail.com>
2023-02-17 21:12:33 -0500

    perf: changed wording for title

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="text-center p-5">
-      <h1 className="text-2xl font-bold text-white">Recession Count Down</h1>
+      <h1 className="text-2xl font-bold text-white">Recession Countdown</h1>
     </div>
   );
 };
