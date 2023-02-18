6e0db7958b4bda3bf70b7ef30219a5bde61d3e39
Rohan Kewalramani <rkewalramani4@gmail.com>
2023-02-18 08:44:53 -0500

    feat: added all parts for stock box

--- src/Box.tsx
+++ src/Box.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BsArrowUpSquareFill } from "react-icons/bs";
 
-const Box = () => {
+const Box = (props: { ticker: string; price: string }) => {
   return (
-    <div className="box-content h-32 w-64 p-4 border-2 rounded-lg border-black">
-      <div className="text-center font-bold text-2xl">TSLA</div>
+    <div className="box-content h-8 w-64 p-4 rounded-lg bg-gray-900 inline-flex">
+      <BsArrowUpSquareFill className="text-4xl" />
+      <div className="text-white text-center font-bold pl-4 text-xl">
+        {props.ticker}
+      </div>
+      <div className="text-center text-white">{props.price}</div>
     </div>
   );
 };
