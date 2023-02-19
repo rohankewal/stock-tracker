f1f74881589f14d1b67b0b11fd02d1f6c09ba1b2
Rohan Kewalramani <rkewalramani4@gmail.com>
2023-02-19 15:41:50 -0500

    perf: fixed component styling and positioning

--- src/BoxRow.tsx
+++ src/BoxRow.tsx
@@ -4,11 +4,13 @@ import Box from "./Box";
 const BoxRow = () => {
   return (
     // TODO: make grid centered on the page
-    <div className="grid grid-cols-4 items-center">
-      <Box ticker="TSLA" price="912.32" />
-      <Box ticker="AAPL" price="912.32" />
-      <Box ticker="AMZN" price="912.32" />
-      <Box ticker="BRCC" price="912.32" />
+    <div className="flex justify-center">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <Box ticker="TSLA" price="912.32" />
+        <Box ticker="AAPL" price="912.32" />
+        <Box ticker="AMZN" price="912.32" />
+        <Box ticker="BRCC" price="912.32" />
+      </div>
     </div>
   );
 };
