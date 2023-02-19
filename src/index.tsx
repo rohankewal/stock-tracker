434642dafabd3d0dfaae8ed1e013aed1b78f9eb6
Rohan Kewalramani <rkewalramani4@gmail.com>
2023-02-19 10:51:09 -0500

    feat: added grid component to page

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Header from "./Header";
+import BoxRow from "./BoxRow";
 import Footer from "./Footer";
 
 const root = ReactDOM.createRoot(
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Header />
+    <BoxRow />
     <Footer />
   </React.StrictMode>
 );
